fd34cdae1ce2989287b9416174f1c2eb0d6f192e
Tyler McKellar <tylermckellar@gmail.com>
2019-03-17 21:01:20 -0400

    Back end code is done

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,114 +1,112 @@
-// const db = require("../models");
+const db = require("../models");
 const request = require("request");
 const cheerio = require("cheerio");
-const Article = require("../models/Article");
-const Note = require("../models/Note");
 
-module.exports = function(app) {
-    app.get("/api/articles", function(req, res) {
-        Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
+module.exports = (app) => {
+    app.get("/api/articles", (req, res) => {
+        db.Article.find({})
+        .then((dbArticle) =>
+            res.json(dbArticle)
+        )
+        .catch((err) =>
+            res.json(err)
+        );
     });
     app.get("/api/scrape", (req, res) => {
         request("https://www.comicbook.com/", (error, response, html) => {
             const $ = cheerio.load(html);
             console.log($("article").length);
-            $("article").each(function(i, element) {
+            $("article").each((i, element) => {
                 let result = {};
 
                 result.title = $(this).find("h3").find("a").text();
                 result.link = $(this).find("h3").find("a").attr("href");
                 result.photo = $(this).find("a").find("img").attr("src");
       
-                Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                });
+                db.Article.create(result)
+                .then((dbArticle) =>
+                    console.log(dbArticle)
+                )
+                .catch((err) =>
+                    console.log(err)
+                );
             });
         res.redirect("/");
         });
     });
 
-    app.get("/api/notes", function(req, res) {
-        Note.find({})
-        .then(function(dbNote) {
-            res.json(dbNote);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
+    app.get("/api/notes", (req, res) => {
+        db.Note.find({})
+        .then((dbNote) =>
+            res.json(dbNote)
+        )
+        .catch((err) =>
+            res.json(err)
+        );
     });
 
-    app.get("/api/articles/:id", function(req, res) {
-        Article.findOne({ _id: req.params.id })
+    app.get("/api/articles/:id", (req, res) => {
+        db.Article.findOne({ _id: req.params.id })
         .populate("note")
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
+        .then((dbArticle) =>
+            res.json(dbArticle)
+        )
+        .catch((err) =>
+            res.json(err)
+        );
     });
 
-    app.post("/api/articles/save/:id", function(req, res) {
-        Article.findOneAndUpdate({ "_id": req.params.id}, {"saved": true})
-        .then(function(dbArticle) {
-            res.send(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
+    app.post("/api/articles/save/:id", (req, res) => {
+        db.Article.findOneAndUpdate({ "_id": req.params.id}, {"saved": true})
+        .then((dbArticle) =>
+            res.send(dbArticle)
+        )
+        .catch((err) =>
+            res.json(err)
+        );
         res.redirect("/");
     });
 
-    app.post("/api/articles/delete/:id", function(req, res) {
-        Article.findOneAndUpdate({ _id: req.params.id}, {saved: false})
-        .then(function(dbArticle) {
+    app.post("/api/articles/delete/:id", (req, res) => {
+        db.Article.findOneAndUpdate({ _id: req.params.id}, {saved: false})
+        .then((dbArticle) => {
             res.redirect("/saved");
         })
-        .catch(function(err) {
-            res.json(err);
-        });
-    });
-
-    app.post("/api/notes/:id", (req, res) => {
-        Note.create(req.body)
-        .then(dbNote => 
-            Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, {new: true})
-        )
-        .then(dbArticle =>
-            res.redirect("/saved")
-        )
         .catch((err) => {
             res.json(err);
         });
     });
 
+    app.post("/api/notes/:id", (req, res) => {
+        db.Note.create(req.body)
+        .then((dbNote) =>
+            db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, {new: true})
+        )
+        .then((dbArticle) =>
+            res.redirect("/saved")
+        )
+        .catch((err) =>
+            res.json(err)
+        );
+    });
+
     app.get("/api/clear", (req, res) => {
-        Article.remove()
+        db.Article.remove()
         .then(() => {
             res.json("documents removed from headline collection");
         });
     });
 
     app.post("/api/notes/delete/:id", (req, res) => {
-        Note.remove({ _id: req.params.id })
-        // .then(dbNote => 
-        //     Article.findOneAndUpdate({ _id: req.params.id }, { $pull: { notes: dbNote._id } }, {new: true})
-        // )
-        .then((dbArticle) => {
-            res.redirect("/saved");
-        })
-        .then((result) => {
-            res.json(result);
-        });
+        db.Note.remove({ _id: req.params.id })
+        .then((dbNote) =>
+            db.Article.findOneAndUpdate({ notes: req.params.id }, { $pull: { notes: req.params.id } }, {new: true})
+        )
+        .then((dbArticle) =>
+            res.redirect("/saved")
+        )
+        .then((result) =>
+            res.json(result)
+        )
     });
 }
--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -3,22 +3,27 @@ const db = require("../models");
 module.exports = (app) => {
     app.get("/", (req, res) => {
         db.Article.find({"saved": false}).sort({_id: 1}).limit(20)
-        .then((dbArticle) => {
-            res.render("index", {data: dbArticle});
-            console.log(dbArticle);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+        .then((dbArticle) =>
+            console.log(dbArticle)
+        )
+        .then((dbArticle) =>
+            res.render("index", {data: dbArticle})
+        )
+        .catch((err) =>
+            res.json(err)
+        );
     });
 
     app.get("/saved", (req, res) => {
         db.Article.find({"saved": true}).sort({_id: 1}).populate("notes", 'body')
-        .then((dbArticle) => {
-            res.render("saved", {article: dbArticle});
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+        .then((dbArticle) =>
+            console.log(dbArticle)
+        )   
+        .then((dbArticle) =>
+            res.render("saved", {article: dbArticle})
+        )
+        .catch((err) =>
+            res.json(err)
+        );
     });
 }
